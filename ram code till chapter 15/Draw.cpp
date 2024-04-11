bb78ac65099688e1a3bfdb220eef65d76dfca4c3
Ramabhadram <40848834+Ramabhadram@users.noreply.github.com>
2024-04-11 11:28:01 -0400

    Add files via upload

---
+++ ram code till chapter 15/Draw.cpp
@@ -0,0 +1,60 @@
+#include "Engine.h"
+
+void Engine::draw()
+{
+	// Rub out the last frame;
+	m_Window.clear(Color::White);
+
+	if (!m_SplitScreen)
+	{
+		// Switch to background view;
+		m_Window.setView(m_BGMainView);
+		// Draw the Background;
+		m_Window.draw(m_BackgroundSprite);
+		// Switch to Main View;
+		m_Window.setView(m_MainView);
+
+		// Draw Thomas;
+		m_Window.draw(m_Thomas.getSprite());
+		// Draw Bob;
+		m_Window.draw(m_Bob.getSprite());
+	}
+	else
+	{
+		// Split Screen View is active;
+		// First Draw  Thomas' Side of the screen;
+		// Switch to Background view;
+		m_Window.setView(m_BGLeftView);
+		// Draw the Background;
+		m_Window.draw(m_BackgroundSprite);
+		// Switch to m_LeftView;
+		m_Window.setView(m_LeftView);
+
+		
+		// Draw Bob;
+		m_Window.draw(m_Bob.getSprite());
+		// Draw Thomas;
+		m_Window.draw(m_Thomas.getSprite());
+
+		// Now draw Bob's side of the screen;
+
+		// Switch to background view;
+		m_Window.setView(m_BGRightView);
+		// Draw the Background;
+		m_Window.draw(m_BackgroundSprite);
+		// Switch to m_RightView;
+		m_Window.setView(m_RightView);
+
+		// Draw Thomas;
+		m_Window.draw(m_Thomas.getSprite());
+		// Draw Bob;
+		m_Window.draw(m_Bob.getSprite());
+	}
+
+	// Draw the HUD;
+	// Switch to m_HudView;
+	m_Window.setView(m_HudView);
+
+	// Show everything we have just drawn;
+	m_Window.display();
+}
